38e2a1f76e7e0fc75fd6cae14a4ae22b5deaeae9
David <dluria13@gmail.com>
2026-09-12 13:47:32 -0400

    Replace debouncer with open logic version

--- examples/push_button/hdl/push_button_top.sv
+++ examples/push_button/hdl/push_button_top.sv
@@ -16,53 +16,41 @@ logic push_button_green_debounced;
 logic push_button_blue_debounced;
 logic push_button_rainbow_debounced;
 
+logic [3:0] debouncers_in, debouncers_out;
+
 logic clk_internal_125;
-logic rst_internal_125_n;
+logic rst_internal_125;
 
-oh_debouncer #( 
-    .BOUNCE(DEBOUNCE_TIME),      // bounce time (s)
-    .FREQUENCY(FREQ_HZ)  // clock frequency (125Mhz)
-) debounce_red (
-    .clk(clk_internal_125), // clock to synchronize to
-    .nreset(rst_internal_125_n), // syncronous active high reset
-    .noisy_in(push_button_red), // noisy input signal to filter
-    .clean_out(push_button_red_debounced) // clean signal to logic
-    );
+assign debouncers_in = {push_button_red, 
+                        push_button_blue, 
+                        push_button_green, 
+                        push_button_rainbow};
+assign push_button_red_debounced = debouncers_out[3];
+assign push_button_blue_debounced = debouncers_out[2];
+assign push_button_green_debounced = debouncers_out[1];
+assign push_button_rainbow_debounced = debouncers_out[0];
 
-oh_debouncer #( 
-    .BOUNCE(DEBOUNCE_TIME),      // bounce time (s)
-    .FREQUENCY(FREQ_HZ)  // clock frequency (125Mhz)
-) debounce_green(
-    .clk(clk_internal_125), // clock to synchronize to
-    .nreset(rst_internal_125_n), // syncronous active high reset
-    .noisy_in(push_button_green), // noisy input signal to filter
-    .clean_out(push_button_green_debounced) // clean signal to logic
-    );
-
-oh_debouncer #( 
-    .BOUNCE(DEBOUNCE_TIME),      // bounce time (s)
-    .FREQUENCY(FREQ_HZ)  // clock frequency (125Mhz)
-) debounce_blue (
-    .clk(clk_internal_125), // clock to synchronize to
-    .nreset(rst_internal_125_n), // syncronous active high reset
-    .noisy_in(push_button_blue), // noisy input signal to filter
-    .clean_out(push_button_blue_debounced) // clean signal to logic
-    );
-
-oh_debouncer #( 
-    .BOUNCE(DEBOUNCE_TIME),      // bounce time (s)
-    .FREQUENCY(FREQ_HZ)  // clock frequency (125Mhz)
-) debounce_rainbow (
-    .clk(clk_internal_125), // clock to synchronize to
-    .nreset(rst_internal_125_n), // syncronous active high reset
-    .noisy_in(push_button_blue), // noisy input signal to filter
-    .clean_out(push_button_blue_debounced) // clean signal to logic
-);
+genvar i;
+generate
+    for (i = 0; i < 4; i++) begin
+        olo_intf_debounce # (
+            .ClkFrequency_g(FREQ_HZ),
+            .DebounceTime_g(DEBOUNCE_TIME)
+        ) debounce (
+            // control signals
+            .Clk(clk_internal_125),
+            .Rst(rst_internal_125),
+            // Input clock domain
+            .DataAsync(debouncers_in[i]),
+            .DataOut(debouncers_out[i])
+        );
+    end
+endgenerate
 
 push_button_bd_wrapper bd (
     .Dout(Dout),
     .clkout_125(clk_internal_125),
-    .rstout_125_n(rst_internal_125_n),
+    .rstout_125(rst_internal_125),
     .push_button_blue(push_button_blue_debounced),
     .push_button_green(push_button_green_debounced),
     .push_button_rainbow(push_button_rainbow_debounced),
